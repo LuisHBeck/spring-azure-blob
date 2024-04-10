30924aa04aab6f357cf9d4d22d40fdec393e3122
LuisHBeck <beck.henrique.luis@gmail.com>
2024-04-10 20:58:43 -0300

    feat: project init

---
+++ azure-blob/src/test/java/br/beck/azureblob/AzureBlobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.beck.azureblob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
